8ed210afa3547cc9588d346df8c028ae3c6b2251
Jason Wu <jaswu@eden.rutgers.edu>
2014-03-15 14:17:06 -0400

    init

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,9 @@
+#!"c:\Users\ibm thinkpad\documents\templates\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==0.6c11','console_scripts','easy_install'
+__requires__ = 'setuptools==0.6c11'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('setuptools==0.6c11', 'console_scripts', 'easy_install')()
+)
