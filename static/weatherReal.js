f35a9a647f4a8f60e388b4a68d4340e2f18aaac4
Jason Wu <jaswu@eden.rutgers.edu>
2014-03-25 22:32:27 -0400

    Updated Atwood Roman

--- static/weatherReal.js
+++ static/weatherReal.js
@@ -1,23 +1,25 @@
 function initializeSecond() {
+	var myLatLng = new google.maps.LatLng({{latitude}}, {{longitude}});
+
 	var mapOptions = {
-		zoom: 10,
-		center: new google.maps.LatLng({{latitude}}, {{longitude}})
+		zoom: 6,
+		center: myLatLng
 	};
 
 	var map = new google.maps.Map(document.getElementById('weather-canvas'), mapOptions);
-	var weatherLayer = new google.maps.weather.WeatherLayer({
-		temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
-	});
-
-	weatherLayer.setMap(map);
-
-	var cloudLayer = new google.maps.weather.CloudLayer();
-	cloudLayer.setMap(map);
-
 	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng({{latitude}}, {{longitude}}),
+		position: myLatLng,
 		map: map,	
 		title: "Your Location Again!"
 	});
+
+	var cloudLayer = new google.maps.weather.CloudLayer();
+	cloudLayer.setMap(map); 
+
+	var weatherLayer = new google.maps.weather.WeatherLayer({
+		temperatureUnits: google.maps.weather.TemperatureUnit.FAHRENHEIT
+	}); 
+
+	weatherLayer.setMap(map);
 }
 google.maps.event.addDomListener(window, 'load', initializeSecond);
